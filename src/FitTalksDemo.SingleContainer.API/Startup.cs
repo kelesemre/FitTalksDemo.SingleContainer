41f87826227d0c36ed77d0b91b91d3d1d2e485d2
emre keles <emre.keles@tomdigital.com.tr>
2022-11-12 14:47:56 +0300

    docker compose edit

--- src/FitTalksDemo.SingleContainer.API/Startup.cs
+++ src/FitTalksDemo.SingleContainer.API/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FitTalksDemo.SingleContainer.API
 {
@@ -39,10 +33,9 @@ namespace FitTalksDemo.SingleContainer.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FitTalksDemo.SingleContainer.API v1"));
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FitTalksDemo.SingleContainer.API v1"));
             app.UseRouting();
 
             app.UseAuthorization();
